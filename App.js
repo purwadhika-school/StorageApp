d38b0d02064d51bc1a538e2e026cc0c73d8e3742
yogieputra8 <yogie.putra8@gmail.com>
2019-01-15 21:45:45 +0700

    setup project

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import Profile from "./src/Profile"
+
+class App extends Component {
+  render() {
+    return <Profile />
+  }
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,11 @@
+import { AppRegistry } from "react-native"
+import App from "./App"
+import { name as appName } from "./app.json"
+import firebase from "@firebase/app"
+import "@firebase/database"
+import { config } from "./config"
+
+
+firebase.initializeApp(config)
+
+AppRegistry.registerComponent(appName, () => App)
